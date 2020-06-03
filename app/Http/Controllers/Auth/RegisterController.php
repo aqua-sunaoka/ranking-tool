9813e79ef9f47dbf001d7b19c870522cd3ceaa21
kyoko.sunaoka <aqua.sunaoka@gmail.com>
2020-06-03 05:34:45 +0000

    user registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';    // リダイレクト先変更 /home→/
 
     /**
      * Create a new controller instance.
